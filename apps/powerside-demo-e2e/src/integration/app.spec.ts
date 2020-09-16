4e0f02f88f496d541a9bda0c62d81fa3b4ca3199
Claude Berube <claude.berube@gmail.com>
2020-09-16 00:08:26 -0400

    upgrade nx fix tests et references css

--- apps/powerside-demo-e2e/src/integration/app.spec.ts
+++ apps/powerside-demo-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('powerside-demo', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to powerside-demo!');
+    getGreeting().contains('powerside-demo app is running!');
   });
 });
--- apps/powerside-demo/src/app/app.component.spec.ts
+++ apps/powerside-demo/src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [AppComponent],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      schemas: [
+        CUSTOM_ELEMENTS_SCHEMA
+      ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
